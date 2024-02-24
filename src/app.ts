4a932b4827a9ec5012fe41d096d203f6a583b412
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-02-24 14:22:48 +0200

    express-test

--- src/app.ts
+++ src/app.ts
@@ -51,10 +51,10 @@ io.on('connection', (socket) => {
 });
 
 io.engine.on('connection_error', (err) => {
-  console.log(err.req); // the request object
-  console.log(err.code); // the error code, for example 1
-  console.log(err.message); // the error message, for example "Session ID unknown"
-  console.log(err.context); // some additional error context
+  console.log('req', err.req); // the request object
+  console.log('code', err.code); // the error code, for example 1
+  console.log('message', err.message); // the error message, for example "Session ID unknown"
+  console.log('context', err.context); // some additional error context
 });
 
 app.get<{}, MessageResponse>('/', (req, res) => {
